1a6deb1c9ee18a409bbfcfff32a20aa02a0c8057
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-09-18 12:02:02 +0300

    twitter share button

--- src/app/components/Share.tsx
+++ src/app/components/Share.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineWhatsApp } from "react-icons/ai";
+import { RiTwitterXLine } from "react-icons/ri";
 import { localDate } from "../utils/localDate";
 import { relativeDate } from "../utils/relativeDate";
 import { earthquakeDataType } from "../types/dataType";
@@ -19,11 +20,19 @@ export default function Share({ data }: propsType) {
         `https://api.whatsapp.com/send?text=${location}%0AMagnitude: ${magnitude}%0ADepth: ${depth}%0A${localdate}%0A${relativedate}%0Ahttps://lastearthquakes.vercel.app/`,
         "_blank"
       );
+    } else if (app == "twitter") {
+      window.open(
+        `https://twitter.com/intent/tweet?text=${location}%0AMagnitude: ${magnitude}%0ADepth: ${depth}%0A${localdate}%0A${relativedate}%0Ahttps://lastearthquakes.vercel.app/`,
+        "_blank"
+      );
     }
   };
   return (
     <div>
-      <button onClick={() => share("whatsapp")}>
+      <button onClick={() => share("twitter")} className="m-0.5 p-1">
+        <RiTwitterXLine className="w-5 h-5" />
+      </button>
+      <button onClick={() => share("whatsapp")} className="m-0.5 p-1">
         <AiOutlineWhatsApp className="w-5 h-5" />
       </button>
     </div>
